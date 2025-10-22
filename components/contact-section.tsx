13ee781d1dad8ceed6e06615856cbf08eb7ef168
NetCode-crypto <negounegou4@gmail.com>
2025-10-22 08:35:11 +0100

    final commit

--- components/contact-section.tsx
+++ components/contact-section.tsx
@@ -11,7 +11,6 @@ import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 import { Label } from "@/components/ui/label"
 import { Sen } from "next/font/google"
-import { SendInviteMail } from "./invit"
 
 export default function ContactSection() {
   const [formData, setFormData] = useState({
